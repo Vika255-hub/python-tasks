da1a705a4c08edb9096832ab65731253cd40f279
Vika255-hub <anisovicVS@mail.ru>
2021-02-11 17:10:33 +0300

    Add new method repaint. Update method close_doors and open_doors. Add new function random_repaint.

--- classes.py
+++ classes.py
@@ -1,16 +1,30 @@
+import random
+
+
 class CAR:
     doors_count = 5
-    color = 'black'
     fuel_consumption = 10
 
-    def __init__(self, fuel=100):
+    def __init__(self, fuel=100, is_doors_open=False, color='black'):
         self.fuel = fuel
+        self.is_doors_open = is_doors_open
+        self.color = color
+
+    @property
+    def _fuel(self):
+        return self.fuel
 
     def open_doors(self):
-        return True
+        if self.is_doors_open == True:
+            raise Exception('Doors Open')
+
+        self.is_doors_open = True
 
     def close_doors(self):
-        return False
+        if self.is_doors_open == False:
+            raise Exception('Doors Close')
+
+        self.is_doors_open = False
 
     def check_fuel(self):
         return self.fuel
@@ -24,17 +38,27 @@ class CAR:
     def add_fuel(self, value):
         self.fuel += value
 
+    def repaint(self, new_color):
+        if self.color == new_color:
+            raise Exception('Equal color')
+
+        self.color = new_color
+
 
 class BMW(CAR):
     doors_count = 2
-    color = 'red'
     fuel_consumption = 15
+
+    def __init__(self, fuel=100, is_doors_open=False, color='red'):
+        super().__init__(fuel, is_doors_open, color)
 
 
 class Skoda(CAR):
     doors_count = 5
-    color = 'white'
     fuel_consumption = 8
+
+    def __init__(self, fuel=100, is_doors_open=False, color='white'):
+        super().__init__(fuel, is_doors_open, color)
 
 
 def compare_fuel():
@@ -53,5 +77,19 @@ def compare_fuel():
     return False
 
 
+def random_repaint():
+    car_bmw = BMW()
+    new_color = random.choice(['black', 'white', 'red', 'yellow', 'blue', 'green', 'purple'])
+
+    try:
+        car_bmw.repaint(new_color)
+    except Exception:
+        return 'Equal color'
+
+    return car_bmw.color
+
+
 if __name__ == '__main__':
     print(compare_fuel())
+    print(random_repaint())
+
